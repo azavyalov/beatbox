55c137072460ef6a201c75cc586eefa918c6d462
azavyalov <alexeysorsa@gmail.com>
2020-09-30 18:04:45 +0300

    Initializing SoundPool depends on the version of Android

--- app/src/main/java/com/azavyalov/beatbox/BeatBox.java
+++ app/src/main/java/com/azavyalov/beatbox/BeatBox.java
@@ -3,8 +3,10 @@ package com.azavyalov.beatbox;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Build;
 import android.util.Log;
 
 import java.io.IOException;
@@ -25,10 +27,25 @@ public class BeatBox {
 
     public BeatBox(Context context) {
         this.mAssets = context.getAssets();
-        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        initSoundPool();
         setPlaybackSpeedRate(1.0f);
         setPlaybackSpeedLabel(context.getString(R.string.playback_label));
         loadSounds();
+    }
+
+    private void initSoundPool() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes attributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            mSoundPool = new SoundPool.Builder()
+                    .setAudioAttributes(attributes)
+                    .setMaxStreams(MAX_SOUNDS)
+                    .build();
+        } else {
+            mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        }
     }
 
     private void loadSounds() {
